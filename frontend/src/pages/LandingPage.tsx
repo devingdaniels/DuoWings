a93461a534275ec9d062a912467741af0475e8b6
Devin Daniels <devingdaniels@gmail.com>
2023-08-07 21:21:49 -0700

    Added landing page styling

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,17 +3,22 @@ import { Link } from "react-router-dom";
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="landing-page-header">
-      <header>
-        <h1>DuoWings</h1>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
-      </header>
-      <div>content</div>
+    <div className="landing-page-container">
+      <div className="landing-page-section-1">
+        <header>
+          <h1>DuoWings</h1>
+          <nav>
+            <Link to="/login">
+              <button className="landing-page-login-button">Sign In</button>
+            </Link>
+          </nav>
+        </header>
+        <div className="call-to-action-container">
+          <Link to="/register">
+            <button className="landing-page-register-button">Sign Up</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
