a9b3b506e06cf91e31197f0366d09e742b5e05fd
Devin Daniels <devingdaniels@gmail.com>
2023-09-24 13:25:29 -0700

    Udpating Deck overview cards with options and edibility

--- frontend/src/pages/deck/DeckOverviewCard.tsx
+++ frontend/src/pages/deck/DeckOverviewCard.tsx
@@ -1,18 +1,37 @@
+import { useState } from "react"; // Import useState
 import { IWordDeck } from "../../interfaces/index";
 import { useNavigate } from "react-router-dom";
+import { SlOptionsVertical } from "react-icons/sl";
 
 const Deck = ({ deck }: { deck: IWordDeck }) => {
   // Hooks
   const navigate = useNavigate();
+  const [isOptionsOpen, setIsOptionsOpen] = useState(false); // State variable to control submenu visibility
+
   const handleDeckClick = (deck: IWordDeck) => {
     navigate(`/vocab/decks/${deck._id}`);
   };
 
+  const handleOptionsClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.stopPropagation();
+    // Toggle the state variable to show/hide the submenu
+    setIsOptionsOpen((prev) => !prev);
+  };
+
   return (
-    <div className="deck-card" onClick={() => handleDeckClick(deck)}>
+    <div className="deck-card">
       <div className="deck-header">
-        <h4>{deck.name}</h4>
         <span className="level">Level {deck.level}</span>
+        <h4 onClick={() => handleDeckClick(deck)}>{deck.name}</h4>
+        <div className="options-container" onClick={handleOptionsClick}>
+          <SlOptionsVertical />
+          {isOptionsOpen && (
+            <div className="submenu">
+              <div className="submenu-option">Edit</div>
+              <div className="submenu-option">Delete</div>
+            </div>
+          )}
+        </div>
       </div>
       <div className="deck-details">
         <p>Description: {deck.description}</p>
