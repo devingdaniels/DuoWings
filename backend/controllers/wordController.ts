24bd0880f75a51e95e98a940a09b1d422f7a89b7
Devin Daniels <devingdaniels@gmail.com>
2023-10-10 00:13:17 -0700

    added comments for future word controller implementation

--- backend/controllers/wordController.ts
+++ backend/controllers/wordController.ts
@@ -5,6 +5,10 @@ import { WordModel } from "../mongodb/models/wordModel";
 const createWord = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     console.log("Backend: req.body:", req.body);
+    // Get the deck  ID from req.body or req.params
+    // Get Deck from DB
+    // Create a new word
+    // Save the word to the DB and add the word to the deck
     res.status(201).json({ message: "Implement creating a new word from the deckIID" });
   } catch (error) {
     console.error("Backend: Error creating deck:", error);
