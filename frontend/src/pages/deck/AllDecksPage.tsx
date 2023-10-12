45666fa75b95726df028182ee5d2e1c1a24575f2
Devin Daniels <devingdaniels@gmail.com>
2023-10-11 23:41:19 -0700

    added comment

--- frontend/src/pages/deck/AllDecksPage.tsx
+++ frontend/src/pages/deck/AllDecksPage.tsx
@@ -14,6 +14,7 @@ import { INewVocabDeck } from "../../interfaces/index";
 import Spinner from "../../utils/Spinner";
 import { FaCreativeCommonsZero } from "react-icons/fa";
 
+// AllDeckComponent
 const AllDecksPage: React.FC = () => {
   // Redux
   const { decks, isSuccess, isLoading } = useAppSelector(
