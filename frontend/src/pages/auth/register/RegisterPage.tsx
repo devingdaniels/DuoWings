2cd32710bbaa3725a964efc2e836e824d559f7c5
Devin Daniels <devingdaniels@gmail.com>
2024-01-25 21:23:25 -0500

    added UI images

--- frontend/src/pages/auth/register/RegisterPage.tsx
+++ frontend/src/pages/auth/register/RegisterPage.tsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import RegisterForm from "./RegisterForm";
-//! Delete this later and turn into a onboard flow
-import GetUserDemographics from "./GetUserDemographics";
+import GetUserDemographics from "./GetUserDemographics"; //! Delete this later and turn into a onboard flow
 
 const RegisterPage: React.FC = () => {
   const navigate = useNavigate();
